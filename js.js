1bea3b4bfeaad404e8c37c6b013cb284f08b4823
SolomonLucaTW <solomonlucatechwear@gmail.com>
2024-11-06 18:07:10 +0800

    Copyright txt size und js modify

--- js.js
+++ js.js
@@ -4,5 +4,4 @@ if(window.innerWidth < window.innerHeight)
     document.querySelector("#page").style.width = "95vw"; //content width
     document.body.style.fontSize = "2.7vh"; //basic text size
     document.getElementById("toptitle").style.fontSize = "15vw";
-    document.getElementById("toptitle").innerHTML="--+--<br>Solomon Luca<br>--+--";
 }
