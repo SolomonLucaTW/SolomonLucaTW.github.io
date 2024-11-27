793e7f94b4294dbfd2a5ae1f34a413adf87f373a
SolomonLucaTW <solomonlucatechwear@gmail.com>
2024-11-27 16:18:51 +0800

    Huge total update (Content + look tweak)

--- js.js
+++ js.js
@@ -2,6 +2,11 @@ if(window.innerWidth < window.innerHeight)
 {
     // document.body.style.fontSize = "5vh";
     document.querySelector("#page").style.width = "95vw"; //content width
-    document.body.style.fontSize = "2.7vh"; //basic text size
+    document.body.style.fontSize = "5.3vw"; //basic text size
+    for (let re = 0; re < document.getElementsByClassName("translate").length; re++) {
+        // const element = document.getElementsByClassName("translate")[re];
+        document.getElementsByClassName("translate")[re].style.fontSize = "3.5vw";
+    }
     document.getElementById("toptitle").style.fontSize = "15vw";
+    document.getElementById("copyright").style.fontSize = "2.5vw";
 }
